e53befe2a8f647ba66aa9a55e67c4dd305060767
David fox <davidfox967@yahoo.com>
2022-09-24 18:29:14 -0600

    created post logic and frontend skeleton

---
+++ src/styles.js
@@ -0,0 +1,35 @@
+const styles = {
+	appBar: {
+		borderRadius: 15,
+		margin: "30px 0",
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	heading: {
+		color: "rgba(0,183,255, 1)",
+	},
+	image: {
+		marginLeft: "15px",
+	},
+};
+
+const imageStyles = {
+	marginLeft: "15px",
+};
+
+const headingStyles = {
+	color: "rgba(0,183,255, 1)",
+};
+
+const appBarStyles = {
+	borderRadius: 15,
+	margin: "30px 0",
+	display: "flex",
+	flexDirection: "row",
+	justifyContent: "center",
+	alignItems: "center",
+};
+
+export { styles };
